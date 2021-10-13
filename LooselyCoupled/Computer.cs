799b7c9b4b8b2a65d39b87c9320d6460cc4d57b9
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-10-13 18:23:02 +0300

    break commit

---
+++ LooselyCoupled/Computer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LooselyCoupled
+{
+    public class Computer
+    {
+        public IMemoryUnit RAM { get; set; }
+        public IStorageDevice Disk { get; set; }
+    }
+    public interface IMemoryUnit
+    {
+
+    }
+    public class RAM : IMemoryUnit
+    {
+        public int Capacity { get; set; }
+        public string Vendor { get; set; }
+    }
+    public interface IStorageDevice
+    {
+
+    }
+    public class HardDisk : IStorageDevice
+    {
+        public int RPM { get; set; }
+    }
+    public class SSD : IStorageDevice
+    {
+
+    }
+}
---
+++ LooselyCoupled/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LooselyCoupled
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer computer = new Computer();
+            computer.Disk = new HardDisk();
+            computer.Disk = new SSD();
+        }
+    }
+}
