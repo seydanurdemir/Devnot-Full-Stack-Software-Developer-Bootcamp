ec958432c6e9f34a4e4101871f4d598b81aca0ad
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-10-15 01:49:17 +0300

    break commit

---
+++ SRP/DBHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP
+{
+    public class DBHelper
+    {
+        SqlConnection sqlConnection;
+
+        public DBHelper(string connectionString)
+        {
+            new SqlConnection(connectionString);
+        }
+    }
+}
--- SRP/Program.cs
+++ SRP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace SRP
 {
@@ -9,7 +10,44 @@ namespace SRP
             /**
              * Bir sınıfı değiştirmek için birden fazla sebebe ihtiyaç duyuyorsanız,
              * bu prensibi ihlal ediyorsunuz demektir.
+             * Her sınıf ve her metot sadece bir işten sorumludur.
              */
+
+            // ihlal eden kod :
+
+            //Console.WriteLine("Urun Adi :");
+            //string name = Console.ReadLine();
+
+            //SqlConnection sqlConnection = new SqlConnection("Data Source=localhost\\SQLEXPRESS;Initial Catalog=NewStoreDB;Integrated Security=True");
+            //SqlCommand sqlCommand = new SqlCommand("INSERT into Products (Name) values (@name)", sqlConnection);
+            //sqlCommand.Parameters.AddWithValue("@name", name);
+            
+            //sqlConnection.Open();
+            //var affectedRow = sqlCommand.ExecuteNonQuery();
+            //sqlConnection.Close();
+            
+            //string result = affectedRow > 0 ? "Kayit Basarili" : "Kayit Basarisiz";
+            //Console.WriteLine(result);
+
+            Console.WriteLine("Urun Adi :");
+            string name = Console.ReadLine();
+            string result = AddProduct(name);
+            Console.WriteLine(result);
+        }
+
+        private static string AddProduct(string name)
+        {
+            SqlConnection sqlConnection = new SqlConnection("Data Source=localhost\\SQLEXPRESS;Initial Catalog=NewStoreDB;Integrated Security=True");
+            SqlCommand sqlCommand = new SqlCommand("INSERT into Products (Name) values (@name)", sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@name", name);
+
+            sqlConnection.Open();
+            var affectedRow = sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+
+            string result = affectedRow > 0 ? "Kayit Basarili" : "Kayit Basarisiz";
+
+            return result;
         }
     }
 }
