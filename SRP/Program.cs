26834e7b71b1fc95d6aa05d7500e918ac5c02657
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-10-14 18:31:33 +0300

    break commit

---
+++ SRP/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SRP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+             * Bir sınıfı değiştirmek için birden fazla sebebe ihtiyaç duyuyorsanız,
+             * bu prensibi ihlal ediyorsunuz demektir.
+             */
+        }
+    }
+}
