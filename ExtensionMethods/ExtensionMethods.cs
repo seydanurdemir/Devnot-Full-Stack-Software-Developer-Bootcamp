fea27e88eb57ba55ade02c7622331a7fd733c15b
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-10-14 18:24:07 +0300

    last changes before new project

---
+++ ExtensionMethods/ExtensionMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethods
+{
+    public static class ExtensionMethods
+    {
+        // Extended method, static and this keywords
+
+        public static double GetSquare(this int number)
+        {
+            return Math.Pow(number, 2);
+        }
+
+        public static string RemoveAllSpaces(this string value)
+        {
+            return value.Replace(" ", string.Empty);
+        }
+
+        //public static int GetTotalWorkDays (this DateTime dateTime, DateTime[] vacations)
+        //{
+        //    return 0;
+        //}
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 8;
+            var value1 = x.GetSquare();
+            Console.WriteLine(value1);
+
+            string y = "Devnot CSharp Bootcamp";
+            var value2 = y.RemoveAllSpaces();
+            Console.WriteLine(value2);
+
+            //int z = DateTime.Now.GetTotalWorkDays(new DateTime[] { });
+            //Console.WriteLine(z);
+
+            var result = new Test().Random?.Next(1, 100);
+            Console.WriteLine(result);
+        }
+    }
+    public class Test
+    {
+        public Random Random { get; set; }
+    }
+}
