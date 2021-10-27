8864bf5f12a0abdaf527e78296d101029370c23e
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-10-28 02:29:05 +0300

    break commit

---
+++ introAPI/Controllers/RestaurantsController.cs
@@ -0,0 +1,94 @@
+﻿using introAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace introAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantsController : ControllerBase
+    {
+        private List<Restaurant> restaurants = new List<Restaurant>
+            {
+                new Restaurant{ Id=1, Name="Kofteci Yusuf"},
+                new Restaurant{ Id=2, Name="Mc Donalds"},
+                new Restaurant{ Id=3, Name="Burger King"}
+            };
+
+        [HttpGet]
+        public IActionResult GetRestaurants()
+        {
+            return Ok(restaurants);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetRestaurant(int id)
+        {
+            var restaurant = restaurants.FirstOrDefault(r => r.Id == id);
+            if (restaurant != null)
+            {
+                return Ok(restaurants);
+            }
+            //return NotFound();
+            return BadRequest(new { Message = $"{id} id'li bir restoran bulunamadi!" });
+        }
+
+        [HttpGet("Search/{cityId}")]
+        public IActionResult GetRestaurantByCity(int cityId)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult AddRestaurant([FromBody]Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                restaurants.Add(restaurant);
+                restaurant.Id = restaurants[restaurants.Count - 1].Id + 1;
+                //StatusCodes.201
+                return CreatedAtAction(nameof(GetRestaurant), new { id = restaurant.Id }, null);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateRestaurant(int id, Restaurant newRestaurant)
+        {
+            if (id > 0)
+            {
+                if (ModelState.IsValid)
+                {
+                    // guncellendi
+                    return Ok(newRestaurant);
+                }
+                return BadRequest(ModelState);
+            }
+            return NotFound();
+        }
+
+        //private bool isExist(int id)
+        //{
+        //    return id % 2 == 0 ? true : false;
+        //}
+
+        [HttpDelete("{id}")]
+        //[HttpDelete()]
+        public IActionResult DeleteRestaurant(int id)
+        //public IActionResult DeleteRestaurant(List<int> id)
+        {
+            // silindi
+            return Ok();
+        }
+
+        [HttpGet("Rest")]
+        public Restaurant GetRestaurant()
+        {
+            return new Restaurant { Id = 4, Name = "Kayseri Mutfagi" };
+        }
+    }
+}
---
+++ introAPI/Models/Restaurant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace introAPI.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Restoran adi belirtilmelidir")]
+        public string Name { get; set; }
+        public double? Score { get; set; }
+        public string Address { get; set; }
+        public int? DeliveryTime { get; set; }
+        public int? CityId { get; set; }
+    }
+}
