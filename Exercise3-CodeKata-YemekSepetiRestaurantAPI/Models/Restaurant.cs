39f5ebcdb6ec2e51d8e079ecfc7464d4cdec4c0b
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-11-12 19:15:23 +0300

    break commit

---
+++ Exercise3-CodeKata-YemekSepetiRestaurantAPI/Models/Restaurant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercise3_CodeKata_YemekSepetiRestaurantAPI.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public double Speed { get; set; }
+        public double Service { get; set; }
+        public double Flavour { get; set; }
+        public double MinDelivery { get; set; }
+        public int DeliveryTimeStart { get; set; }
+        public int DeliveryTimeEnd { get; set; }
+    }
+}
