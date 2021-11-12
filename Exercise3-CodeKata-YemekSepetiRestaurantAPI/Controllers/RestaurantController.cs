40c9071155dbf1f13dcf5078addf7d17846d0896
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-11-13 02:27:04 +0300

    break commit

--- Exercise3-CodeKata-YemekSepetiRestaurantAPI/Controllers/RestaurantController.cs
+++ Exercise3-CodeKata-YemekSepetiRestaurantAPI/Controllers/RestaurantController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Exercise3_CodeKata_YemekSepetiRestaurantAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +12,68 @@ namespace Exercise3_CodeKata_YemekSepetiRestaurantAPI.Controllers
     [ApiController]
     public class RestaurantController : ControllerBase
     {
+        private List<Restaurant> restaurants = new List<Restaurant>
+        {
+            new Restaurant { Id = 0, Name = "Pizza Bulls", City = "Istanbul", Region = "Tuzla-Icmeler-Mah", MinDelivery = 19.90, DeliveryTimeMin = 30, DeliveryTimeMax = 40 },
+            new Restaurant { Id = 1, Name = "Little Caesars Pizza", City = "Kocaeli", Region = "Darica-Baglarbasi-Mah", MinDelivery = 24.90, DeliveryTimeMin = 20, DeliveryTimeMax = 30 },
+            new Restaurant { Id = 2, Name = "Oses Cig Kofte", City = "Kocaeli", Region = "Darica-Zincirlikuyu-Mah", MinDelivery = 20.00, DeliveryTimeMin = 20, DeliveryTimeMax = 30 }
+         };
+
+        [HttpGet("All")]
+
+        public IActionResult GetRestaurants()
+        {
+            return Ok(restaurants);
+        }
+
+        [HttpGet("Id/{id}")]
+
+        public IActionResult GetRestaurantById(int id)
+        {
+            Restaurant restaurant = restaurants.FirstOrDefault(r => r.Id == id);
+            if (restaurant != null)
+            {
+                return Ok(restaurant);
+            }
+            return BadRequest(new { Message = "There is no any restaurant by this id."});
+        }
+
+        [HttpGet("City/{city}")]
+
+        public IActionResult GetRestaurantsByCity(string city)
+        {
+            List<Restaurant> newRestaurantsByCity = new List<Restaurant>();
+            foreach(Restaurant restaurant in restaurants)
+            {
+                if (restaurant.City == city)
+                {
+                    newRestaurantsByCity.Add(restaurant);
+                }
+            }
+            if (newRestaurantsByCity.Count > 0)
+            {
+                return Ok(newRestaurantsByCity);
+            }
+            return BadRequest(new { Message = "There is no any restaurant in this city." });
+        }
+
+        [HttpGet("Region/{region}")]
+
+        public IActionResult GetRestaurantsByRegion(string region)
+        {
+            List<Restaurant> newRestaurantsByRegion = new List<Restaurant>();
+            foreach (Restaurant restaurant in restaurants)
+            {
+                if (restaurant.Region == region)
+                {
+                    newRestaurantsByRegion.Add(restaurant);
+                }
+            }
+            if (newRestaurantsByRegion.Count > 0)
+            {
+                return Ok(newRestaurantsByRegion);
+            }
+            return BadRequest(new { Message = "There is no any restaurant in this region." });
+        }
     }
 }
--- Exercise3-CodeKata-YemekSepetiRestaurantAPI/Models/Restaurant.cs
+++ Exercise3-CodeKata-YemekSepetiRestaurantAPI/Models/Restaurant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,14 +9,20 @@ namespace Exercise3_CodeKata_YemekSepetiRestaurantAPI.Models
     public class Restaurant
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Required!")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Required!")]
         public string City { get; set; }
+        [Required(ErrorMessage = "Required!")]
         public string Region { get; set; }
         public double Speed { get; set; }
         public double Service { get; set; }
         public double Flavour { get; set; }
+        [Required(ErrorMessage = "Required!")]
         public double MinDelivery { get; set; }
-        public int DeliveryTimeStart { get; set; }
-        public int DeliveryTimeEnd { get; set; }
+        [Required(ErrorMessage = "Required!")]
+        public int DeliveryTimeMin { get; set; }
+        [Required(ErrorMessage = "Required!")]
+        public int DeliveryTimeMax { get; set; }
     }
 }
