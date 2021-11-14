a2981fb9b60b440744466ec38fa2fe83a611f374
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-11-14 03:55:56 +0300

    break commit

--- customMiddleware/Startup.cs
+++ customMiddleware/Startup.cs
@@ -39,6 +39,7 @@ namespace customMiddleware
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // First Middleware
             //app.Run(async (context) =>
             //{
             //    await context.Response.WriteAsync("Mudahale ettik. Pipeline durdu.");
